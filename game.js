2ae17c61615f6293170878d5658a89e3f548ca4c
Marek Krzyżowski <themarkcross@gmail.com>
2022-10-18 20:31:19 +0200

    new snake file

--- game.js
+++ game.js
@@ -1,10 +1,28 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from './snake.js';
+
 let lastRenderTime = 0;
-console.log('siema');
+
 function main(currentTime) {
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
   lastRenderTime = currentTime;
-  console.log(secondsSinceLastRender);
+  //console.log(secondsSinceLastRender);
+
+  update();
+  draw();
 }
 
 window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+function draw() {
+  drawSnake();
+}
---
+++ snake.js
@@ -0,0 +1,5 @@
+const SNAKE_SPEED = 2;
+function update() {}
+function draw() {
+  console.log('drwa sbaje');
+}
